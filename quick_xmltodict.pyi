2599dbb00dd822108b1f05435b09f5d0b142e6cb
Phil Krylov <phil@krylov.eu>
2025-08-21 22:59:03 +0200

    feat: Support UTF-8 encoded `bytes` input

--- quick_xmltodict.pyi
+++ quick_xmltodict.pyi
@@ -1,4 +1,4 @@
 from typing import Any
 
-def parse(xml: str) -> dict[str, Any]:
-    """Parse an XML string and convert it into a dictionary."""
+def parse(xml: str | bytes) -> dict[str, Any]:
+    """Parse an XML string or a UTF-8 encoded bytes object and convert it into a dictionary."""
